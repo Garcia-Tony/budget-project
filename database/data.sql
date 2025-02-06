2e73124db635c958859f08758c4281d62bfadfd9
Anthony Garcia <166793944+Garcia-Tony@users.noreply.github.com>
2025-02-06 05:30:05 +0000

    main

--- database/data.sql
+++ database/data.sql
@@ -1,11 +1,10 @@
--- Use SQL insert statements to add any
--- starting/dummy data to your database tables
+INSERT INTO "users" ("username", "passwordHash", "createdAt", "updatedAt")
+VALUES
+('Anthony', '1234', '2025-02-05', '2025-02-05'),
+('Larry', '0000', '2025-02-05', '2025-02-05');
 
--- EXAMPLE:
-
---  insert into "todos"
---    ("task", "isCompleted")
---    values
---      ('Learn to code', false),
---      ('Build projects', false),
---      ('Get a job', false);
+INSERT INTO "expenses" ("userId", "category", "amount", "expenseDate", "recurringDate")
+VALUES
+(1, 'Rent', 1200, '01-01-2025', '2025-02-01'),
+(1, 'Food', 30, '01-10-2025', NULL),
+(2, 'Rent', 2000, '01-01-2025', '2025-02-01');
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,24 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "users" (
+  "userId" SERIAL PRIMARY KEY,
+  "username" varchar,
+  "passwordHash" varchar,
+  "createdAt" varchar,
+  "updatedAt" varchar
+);
+
+CREATE TABLE "expenses" (
+  "expenseId" SERIAL PRIMARY KEY,
+  "userId" int,
+  "category" varchar,
+  "amount" numeric,
+  "expenseDate" date,
+  "recurringDate" date,
+  "createdAt" varchar,
+  "updatedAt" varchar
+);
+
+ALTER TABLE "expenses" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
