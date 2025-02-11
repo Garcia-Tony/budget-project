6e346431c8ce90bcc6d1f8fbc11b8e5555efbca2
Anthony Garcia <166793944+Garcia-Tony@users.noreply.github.com>
2025-02-11 02:24:42 +0000

    Functionality for password bar

--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -3,13 +3,26 @@ export function RegistrationForm() {
     <div className="container">
       <div className="flex flex-wrap mb-1">
         <div className="w-1/2">
-          <h2 className="text-xl font-bold">Sign Up</h2>
+          <h2 className="text-xl font-bold text-white-500">Sign Up</h2>
           <label className="mb-1 block">
-            Username
+            <span className="underline">Username</span>
             <input
               required
               name="username"
+              placeholder="username"
               type="text"
+              className="block border border-gray-600 rounded p-2 h-8 w-full mb-2"
+            />
+          </label>
+          <br />
+          <br />
+          <label className="mb-1 block">
+            Password
+            <input
+              required
+              name="password"
+              placeholder="password"
+              type="password"
               className="block border border-gray-600 rounded p-2 h-8 w-full mb-2"
             />
           </label>
