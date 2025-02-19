89ce8acf15d7460b2c5e275c0fdc82d417ceb754
Anthony Garcia <166793944+Garcia-Tony@users.noreply.github.com>
2025-02-19 03:34:55 +0000

    Change menu bar and add icon

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -10,13 +10,13 @@ export function Home() {
   return (
     <div className="relative flex-grow flex-1 px-4">
       <button
-        className="border rounded py-1 px-3 bg-black text-white hover:bg-white transition"
+        className="rounded py-1 px-3 bg-white text-black hover:bg-gray-200 transition"
         onClick={() => setIsMenuOpen(!isMenuOpen)}>
-        Menu
+        <img src="/menu.svg" alt="Menu" className="w-6 h-6" />
       </button>
 
       {isMenuOpen && (
-        <div className="absolute top-12 left-4 bg-white shadow-md border rounded p-2">
+        <div className="absolute top-12 left-4 bg-white shadow-md border rounded p-2 z-50">
           <button
             className="block text-center border rounded py-1 px-3 bg-blue-600 text-white w-full"
             onClick={() => {
