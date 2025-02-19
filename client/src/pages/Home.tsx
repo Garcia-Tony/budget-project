9bf5af6fa831231258e0e8aebefbf29cca40d74f
Anthony Garcia <166793944+Garcia-Tony@users.noreply.github.com>
2025-02-19 04:36:14 +0000

    Add ProBudget logo

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -18,16 +18,19 @@ export function Home() {
 
   return (
     <div className="relative flex-grow flex-1 px-4">
-      <button
-        className="rounded py-1 px-3 bg-white hover:bg-gray-200 transition"
-        onClick={() => setIsMenuOpen(!isMenuOpen)}>
-        <svg className="w-20 h-20 text-[#01898B]" viewBox="0 0 448 512">
-          <path
-            fill="currentColor"
-            d="M16 132h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
-          />
-        </svg>
-      </button>
+      <div className="flex items-center space-x-4">
+        <button
+          className="rounded py-1 px-3 bg-white hover:bg-gray-200 transition"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          <svg className="w-20 h-20 text-[#01898B]" viewBox="0 0 448 512">
+            <path
+              fill="currentColor"
+              d="M16 132h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
+            />
+          </svg>
+        </button>
+        <img src="/ProBudget.png" alt="Pro Budget Logo" className="w-16 h-16" />
+      </div>
 
       {isMenuOpen && (
         <div className="absolute top-12 left-4 bg-white shadow-md border rounded p-2 z-50">
