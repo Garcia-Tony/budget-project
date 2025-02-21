56eada787f40af6e92308f32cd705b35dffb7e56
Anthony Garcia <166793944+Garcia-Tony@users.noreply.github.com>
2025-02-21 03:02:45 +0000

    Create the desktop version of login page

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -45,44 +45,55 @@ export function SignInForm() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen w-full bg-[#13878B] absolute top-0 left-0">
-      <img src="/ProBudget.png" alt="ProBudget Logo" className="w-32 mb-4" />
-      <h2 className="text-6xl text-black text-center mb-6">Login</h2>
+      <img
+        src="/ProBudget.png"
+        alt="ProBudget Logo"
+        className="md:justify-center md:w-36 md:mb-8 w-32 mb-4"
+      />
       <form onSubmit={handleSubmit} className="w-full max-w-sm">
-        <div className="mb-1">
-          <label className="w-1/2">
-            <span className="underline text-2xl text-white"> Username </span>
+        <h2 className="font-bold text-6xl text-black text-center mb-6 md:text-7xl">
+          Login
+        </h2>
+        <div className="mb-1 md:flex justify-center">
+          <label className="mb-1 block">
+            <span className="md:text-3xl underline text-2xl text-white">
+              {' '}
+              Username{' '}
+            </span>
             <input
               required
               name="username"
               placeholder="Username"
               type="text"
-              className="text-2xl block border border-gray-600 rounded p-2 h-12 w-full mb-2 mt-2"
+              className="text-2xl block border border-gray-600 rounded p-2 h-12 w-full md:w-[500px] lg:w-[600px] mb-2 mt-2"
             />
           </label>
         </div>
         <br></br>
-        <div className="mb-1">
+        <div className="mb-1 md:flex justify-center">
           <label className="mb-1 block">
-            <span className="underline text-2xl text-white">Password</span>
+            <span className="md:text-3xl underline text-2xl text-white">
+              Password
+            </span>
             <input
               required
               name="password"
               placeholder="Password"
               type="password"
-              className=" text-2xl block border border-gray-600 rounded p-2 h-12 w-full mb-2 mt-2"
+              className=" text-2xl block border border-gray-600 rounded p-2 h-12 w-full md:w-[500px] lg:w-[600px] mb-2 mt-2"
             />
           </label>
         </div>
         <button
           disabled={isLoading}
-          className="text-2xl px-20 text-center border rounded-full py-3 bg-white text-black mt-12 mx-auto block hover:bg-gray-200 transition">
+          className="md:mt-8 md:md:flex justify-center text-2xl px-20 text-center border rounded-full py-3 bg-white text-black mt-12 mx-auto block hover:bg-gray-200 transition">
           Login
         </button>
       </form>
-      <p className="text-xl mt-8 text-white">
+      <p className="text-xl mt-8 text-white md:text-2xl">
         Don't have an account?{' '}
         <span
-          className="text-xl text-black cursor-pointer underline hover:text-[#00C3C9] transition"
+          className="md:text-2xl text-xl text-black cursor-pointer underline hover:text-[#00C3C9] transition"
           onClick={() => navigate('/sign-up')}>
           Sign Up
         </span>
