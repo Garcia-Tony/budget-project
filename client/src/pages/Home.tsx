5afa6a7da5be8955e8204a01ef95e2aa0346bbd6
Anthony Garcia <166793944+Garcia-Tony@users.noreply.github.com>
2025-02-22 21:45:44 +0000

    Create a add icon button and have it be functional to load a popup

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -33,10 +33,33 @@ export function Home() {
           alt="Pro Budget Logo"
           className="size-14 max-w-[60px] max-h-[60px] mt-5 md:size-20 md:mt-4 md:max-w-[150px] md:max-h-[150px]"
         />
-        <div className="flex w-full justify-center md:mt-4">
+        <div className="flex w-full justify-between items-center md:mt-4">
           <h2 className="md:text-6xl text-4xl font-bold text-black ml-7 mr-40 mt-4 md:mb-4 ">
             Expenses
           </h2>
+
+          <button>
+            <svg
+              className="ml-[-50px] md:ml-[100px] md:w-[50px] md:h-[50px] mt-4 md:mb-4 w-12 h-12 text-[#01898B]"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M8 12H12M16 12H12M12 12V8M12 12V16"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
         </div>
       </div>
 
